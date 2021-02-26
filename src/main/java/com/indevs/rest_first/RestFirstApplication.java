73bdef673c66abd463fb149d803be3094374c2b6
Nastya <creepystasy97@gmail.com>
2021-02-26 16:17:05 +0400

    first commit

---
+++ src/main/java/com/indevs/rest_first/RestFirstApplication.java
@@ -0,0 +1,13 @@
+package com.indevs.rest_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/indevs/rest_first/RestFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indevs.rest_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
